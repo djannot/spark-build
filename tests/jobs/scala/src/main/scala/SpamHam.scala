1e7fbf46e7ba9b28c25e1332bf43ad87f07bdf5a
Denis Jannot <djannot@gmail.com>
2018-04-16 12:46:36 +0200

    Update SpamHam.scala

--- tests/jobs/scala/src/main/scala/SpamHam.scala
+++ tests/jobs/scala/src/main/scala/SpamHam.scala
@@ -1,7 +1,6 @@
 import scala.util.Random
 import java.util
 
-import spark.implicits._
 import org.apache.spark.ml.classification.NaiveBayes
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature._
@@ -142,7 +141,7 @@ object Spammer {
 
     val allMessages = spark.sparkContext.textFile(labeledMessages)
     val spamCounts = cullWordCounts(allMessages, "spam")
-println(spamCounts.toDF().show())
+spamCounts.collect().foreach(println)
     val hamCounts = cullWordCounts(allMessages, "ham")
 
     // The kafka producer requires a Java HashMap
